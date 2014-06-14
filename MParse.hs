d77b36d1a163fd04827be1a8362b44621d9f2b04
kongo2002 <kongo2002@googlemail.com>
2014-06-14 23:27:34 +0200

    add some preliminary aggregation logic

--- MParse.hs
+++ MParse.hs
@@ -5,12 +5,13 @@ module MParse where
 import           Control.Applicative
 import           Data.Attoparsec.ByteString.Char8
 import qualified Data.ByteString.Char8 as BS
-import           Data.Maybe         ( catMaybes )
+import           Data.List          ( find )
+import qualified Data.Map as M
 import           Data.Time
-import           System.Locale      ( defaultTimeLocale )
 import           System.Environment ( getArgs )
 
 import MParse.Parser
+import MParse.Types
 
 
 getCurrentYear :: IO Integer
@@ -19,14 +20,37 @@ getCurrentYear = do
   return year
 
 
+aggregate :: [LogLine] -> M.Map MongoElement (Integer, Integer)
+aggregate ls =
+  M.fromListWith group ls'
+ where
+  group (_, 0) acc = acc
+  group (_, t) (c', t') = (c'+1, t+t')
+
+  ls' = [(qiQuery qi, (1, getMs (qiInfos qi))) | LogLine _ _ (LcQuery qi) <- ls]
+
+
+getMs :: [CommandInfo] -> Integer
+getMs cs =
+  get $ find ms cs
+ where
+  ms (CiRuntime _) = True
+  ms _             = False
+
+  get (Just (CiRuntime x)) = x
+  get _                    = 0
+
+
 main :: IO ()
 main = do
   -- TODO: proper argument parsing
   [file]   <- getArgs
   thisYear <- getCurrentYear
+  llines   <- parseOnly (loglines thisYear) <$> BS.readFile file
 
-  let parse' = loglines thisYear
+  case llines of
+    Right ls -> print $ aggregate ls
+    Left _   -> putStrLn "failed to parse log file"
 
-  print =<< parseOnly parse' <$> BS.readFile file
 
 -- vim: set et sw=2 sts=2 tw=80:
