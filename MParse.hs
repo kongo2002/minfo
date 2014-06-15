d0dc1689361052ffa584fa4a83b1296d93b5a130
kongo2002 <kongo2002@googlemail.com>
2014-06-15 15:49:59 +0200

    adjust aggregation function

--- MParse.hs
+++ MParse.hs
@@ -3,6 +3,7 @@
 module MParse where
 
 import           Control.Applicative
+import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as BL
 import           Data.List          ( find )
 import qualified Data.Map.Strict as M
@@ -13,31 +14,38 @@ import MParse.Parser
 import MParse.Types
 
 
+type MapKey   = (BS.ByteString, MongoElement)
+type QueryMap = M.Map MapKey (Int, [Int])
+
+
 getCurrentYear :: IO Integer
 getCurrentYear = do
   (year, _, _) <- fmap (toGregorian . utctDay) getCurrentTime
   return year
 
 
-aggregate :: [LogLine] -> M.Map MongoElement (Int, Int)
+aggregate :: [LogLine] -> QueryMap
 aggregate ls =
   M.fromListWith group ls'
  where
-  group (_, 0) acc = acc
-  group (_, t) (c', t') = (c'+1, t+t')
+  -- calculate length on-the-fly
+  group (_, [x]) (c, acc) = (c+1, x:acc)
+  group _ acc   = acc
 
-  ls' = [(qiQuery qi, (1, getMs (qiInfos qi))) | LogLine _ _ (LcQuery qi) <- ls]
+  key qi  = (qiNamespace qi, qiQuery qi)
+  acc0 qi = (key qi, (1, getMs (qiInfos qi)))
+  ls'     = [acc0 qi | LogLine _ _ (LcQuery qi) <- ls]
 
 
-getMs :: [CommandInfo] -> Int
+getMs :: [CommandInfo] -> [Int]
 getMs cs =
   get $ find ms cs
  where
   ms (CiRuntime _) = True
   ms _             = False
 
-  get (Just (CiRuntime x)) = x
-  get _                    = 0
+  get (Just (CiRuntime x)) = [x]
+  get _                    = []
 
 
 main :: IO ()
