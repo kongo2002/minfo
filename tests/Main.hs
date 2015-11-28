2e6f7521b92414868a2c61fd0e4766d970bfa9c8
kongo2002 <kongo2002@googlemail.com>
2015-11-28 20:30:19 +0000

    add some query/aggregate tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -36,6 +36,29 @@ parsed str =
   in  length $ parseFile info str
 
 
+one :: BL.ByteString -> LogContent
+one str =
+  let info = ParserInfo (Nothing, Nothing) (const True) 2015
+  in  lContent $ head $ parseFile info str
+
+
+query :: BL.ByteString -> MongoElement
+query str =
+  case one str of
+    LcQuery q -> qiQuery q
+    other     -> error $ "expected query, but got: " ++ show other
+
+
+pipeline :: BL.ByteString -> [MongoElement]
+pipeline str =
+  case one str of
+    LcAggregate a ->
+      case aiPipeline a of
+        MList ls -> ls
+        other    -> error $ "expected pipeline, but got: " ++ show other
+    other         -> error $ "expected aggregate, but got: " ++ show other
+
+
 tests :: [Test]
 tests =
   [ testGroup "BSON parsing"
@@ -73,6 +96,10 @@ tests =
       ]
     , testGroup "3.x format"
       [ testCase "initandlisten" (1 @=? parsed "2015-10-18T20:22:20.825+0000 I CONTROL  [initandlisten] allocator: tcmalloc\n")
+      , testCase "query #1" (1 @=? parsed "2015-11-28T19:40:37.830+0000 I QUERY    [conn8812] query stats.snapshots query: { v: { $in: [ 25252.0, 2562352.0 ] } } planSummary: COLLSCAN ntoreturn:0 ntoskip:0 nscanned:0 nscannedObjects:273788 keyUpdates:0 writeConflicts:0 numYields:2138 nreturned:0 reslen:20 locks:{ Global: { acquireCount: { r: 4278 } }, Database: { acquireCount: { r: 2139 } }, Collection: { acquireCount: { r: 2139 } } } 110ms\n")
+      , testCase "query #2" (MObject [(MKey "v", MObject [(MOperator In, MList [MValue])])] @=? query "2015-11-28T19:40:37.830+0000 I QUERY    [conn8812] query stats.snapshots query: { v: { $in: [ 25252.0, 2562352.0 ] } } planSummary: COLLSCAN ntoreturn:0 ntoskip:0 nscanned:0 nscannedObjects:273788 keyUpdates:0 writeConflicts:0 numYields:2138 nreturned:0 reslen:20 locks:{ Global: { acquireCount: { r: 4278 } }, Database: { acquireCount: { r: 2139 } }, Collection: { acquireCount: { r: 2139 } } } 110ms\n")
+      , testCase "aggregate #1" (1 @=? parsed "2015-11-28T20:00:12.053+0000 I COMMAND  [conn8818] command stats.$cmd command: aggregate { aggregate: \"snapshots\", pipeline: [ { $match: { k: \"MemoryUsage\", m: /^mono/ } }, { $group: { _id: \"$t\", res: { $push: { m: \"$m\", v: \"$v\" } }, sum: { $sum: \"$v\" } } }, { $sort: { _id: 1.0 } } ], cursor: {} } cursorid:13364525650 keyUpdates:0 writeConflicts:0 numYields:312 reslen:10093 locks:{ Global: { acquireCount: { r: 630 } }, Database: { acquireCount: { r: 315 } }, Collection: { acquireCount: { r: 315 } } } 110ms\n")
+      , testCase "aggregate #2" (3 @=? (length $ pipeline "2015-11-28T20:00:12.053+0000 I COMMAND  [conn8818] command stats.$cmd command: aggregate { aggregate: \"snapshots\", pipeline: [ { $match: { k: \"MemoryUsage\", m: /^mono/ } }, { $group: { _id: \"$t\", res: { $push: { m: \"$m\", v: \"$v\" } }, sum: { $sum: \"$v\" } } }, { $sort: { _id: 1.0 } } ], cursor: {} } cursorid:13364525650 keyUpdates:0 writeConflicts:0 numYields:312 reslen:10093 locks:{ Global: { acquireCount: { r: 630 } }, Database: { acquireCount: { r: 315 } }, Collection: { acquireCount: { r: 315 } } } 110ms\n"))
       ]
     ]
   ]
